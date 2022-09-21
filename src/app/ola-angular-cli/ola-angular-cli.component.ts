b41bebe61bac41e1d7c21df838a9444fbe405c4b
Karamazov <samuel.silveira01@fatec.gov.sp.br>
2022-09-21 18:16:42 -0300

    feat(apostila04): componente criado e importacao de biblioteca
    Adicionado novo componente via automacao CLI e importacao de bootstrap
    (CSS e JS)

---
+++ src/app/ola-angular-cli/ola-angular-cli.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ola-angular-cli',
+  templateUrl: './ola-angular-cli.component.html',
+  styleUrls: ['./ola-angular-cli.component.css']
+})
+export class OlaAngularCliComponent {
+
+
+
+}
